82c8a9ad834e25082624f9f525f47d9071990480
ghong25 <genehong25@gmail.com>
2020-04-03 00:58:42 -0400

    current and hourly weather

--- src/main/java/javaWeather.java
+++ src/main/java/javaWeather.java
@@ -1,7 +1,10 @@
 import java.io.*;
+import java.lang.*;
 import java.net.*;
-import java.util.Scanner;
-import org.json.JSONObject;
+import java.util.*;
+import org.json.*;
+import java.time.*;
+import java.text.SimpleDateFormat;
 
 public class javaWeather {
 
@@ -14,18 +17,17 @@ public class javaWeather {
         Double longitude = 0.00;
         Double latitude = 0.00;
 
+        // get IP address
         try {
             URL ipAddress = new URL("http://checkip.amazonaws.com");
             BufferedReader in = new BufferedReader(new InputStreamReader(ipAddress.openStream()));
 
             ip = in.readLine();
 
-            // get corresponding zip code for IP address from ipapi
+            // get corresponding zip code for IP address from ipApi
             try {
                 URL ipAPI = new URL("https://ipapi.co/" + ip + "/json");
                 HttpURLConnection con = (HttpURLConnection) ipAPI.openConnection();
-                System.out.println("Sending 'GET' request to URL : " + ipAPI);
-                System.out.println("Response Code : " + con.getResponseCode());
 
                 BufferedReader input = new BufferedReader(new InputStreamReader(con.getInputStream()));
                 StringBuffer ipResponse = new StringBuffer();
@@ -83,7 +85,11 @@ public class javaWeather {
             // read JSON response
             JSONObject darkSkyResponse = new JSONObject(responseBuffer.toString());
 
-            System.out.println(darkSkyResponse);
+            //printJsonObject(darkSkyResponse);
+
+            printCurrentWeather(darkSkyResponse);
+
+            printHourlyWeather(darkSkyResponse);
 
             inputBuffer.close();
 
@@ -91,12 +97,67 @@ public class javaWeather {
             e.printStackTrace();
         }
 
+    }
+
+    public static void printCurrentWeather(JSONObject weatherReport) {
+        ArrayList<String> currentWeather = new ArrayList<>();
+
+        JSONObject current = weatherReport.getJSONObject("currently");
+
+        // convert epoch time stamp to local time
+
+        Long milliTime = Long.parseLong(current.get("time").toString()) * 1000;
+        Date epochTime = new Date(milliTime);
+
+        currentWeather.add(epochTime.toString());
+        currentWeather.add("summary: " + current.get("summary").toString());
+        currentWeather.add("wind speed: " + current.get("windGust").toString());
+        currentWeather.add("temperature: " + current.get("temperature").toString());
+        currentWeather.add("apparent temperature: " + current.get("apparentTemperature").toString());
+
+        System.out.println(currentWeather);
 
     }
+
+    public static void printHourlyWeather(JSONObject weatherReport) {
+        String[][] hourWeather = new String[12][5];
+
+        JSONArray hourlyWeather = weatherReport.getJSONObject("hourly").getJSONArray("data");
+
+        // get weather reports for the next 12 hours
+        for (int n = 0; n < 12; n++) {
+            JSONObject hour = (JSONObject) hourlyWeather.get(n);
+
+            Long milliTime = Long.parseLong(hour.get("time").toString()) * 1000;
+            Date epochTime = new Date(milliTime);
+
+            SimpleDateFormat sdf = new SimpleDateFormat("MMMM d, yyyy h:mm a", Locale.ENGLISH);
+            sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+            String formattedDate = sdf.format(epochTime);
+
+            hourWeather[n] = new String[] {
+
+                    formattedDate,
+                    hour.get("summary").toString(),
+                    "Rain %: " + hour.get("precipProbability").toString(),
+                    "feels like: " + hour.get("apparentTemperature").toString(),
+                    "temperature: " + hour.get("temperature").toString()
+            };
+
+            System.out.println(Arrays.toString(hourWeather[n]));
+        }
+
+    }
+
+    public static void printJsonObject(JSONObject jsonObj) {
+        jsonObj.keySet().forEach(keyStr ->
+        {
+            Object keyvalue = jsonObj.get(keyStr);
+            System.out.println("key: "+ keyStr + "        value: " + keyvalue);
+
+            //for nested objects iteration if required
+            //if (keyvalue instanceof JSONObject)
+            //    printJsonObject((JSONObject)keyvalue);
+        });
+    }
 }
-
-// get ip address of computer running program
-
-// get zip code from ip
-
-// get weather from zip code
